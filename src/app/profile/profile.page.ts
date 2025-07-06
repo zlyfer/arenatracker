1d50e89534912e2dc7e58630cfa704655bb3e911
Frederik Shull <zlyfer@pm.me>
2025-07-06 23:25:39 +0200

    update

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -154,4 +154,8 @@ export class ProfilePage implements OnInit, OnDestroy {
   getAvailableChampions(): number {
     return this.champions.length;
   }
+
+  getReversedChampions(): string[] {
+    return this.user?.champs ? [...this.user.champs].reverse() : [];
+  }
 }
